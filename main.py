b57cdaf36e51fd9715c9312cb7069a582ca75c86
Lucas Santos <lucassnts963@gmail.com>
2024-02-22 23:19:18 -0300

    sending project

---
+++ main.py
@@ -0,0 +1,16 @@
+import datetime
+
+from pdf_generator import generate_pdf
+from handler_data import get_data
+
+materials = get_data('D3-2900-04-T-1001')
+
+data = {
+        'company': 'Montisol',
+        'request_number': '0088',
+        'date': datetime.datetime.today().strftime("%d/%m/%Y"),
+        'materials': materials,
+        # ... other template data
+    }
+
+generate_pdf(data)
---
+++ utils.py
@@ -0,0 +1,3 @@
+import math
+def round_to_nearest_multiple_of_6(number):
+    return math.ceil(number / 6) * 6
